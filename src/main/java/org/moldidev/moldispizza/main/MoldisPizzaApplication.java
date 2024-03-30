04bb01ccb418f709ad64a8672b4d26023739d09e
moldy123 <moldovan.co.darius@student.utcluj.ro>
2024-03-30 16:26:46 +0200

    Initial commit

---
+++ src/main/java/org/moldidev/moldispizza/main/MoldisPizzaApplication.java
@@ -0,0 +1,11 @@
+package org.moldidev.moldispizza.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoldisPizzaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoldisPizzaApplication.class, args);
+    }
+}
---
+++ src/test/java/org/moldidev/moldispizza/MoldisPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moldidev.moldispizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoldisPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
